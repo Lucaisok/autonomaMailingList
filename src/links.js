66c5e0b4ac4d94fc08e01dfe82dcdac8ed51cc87
Luca Tomarelli <lucatomarelli1@gmail.com>
2021-04-07 17:38:04 +0200

    fix

--- src/links.js
+++ src/links.js
@@ -7,12 +7,12 @@ export default function Links() {
                 <img src="/images/logoAutonoma%20copy.png" />
             </div>
             <div className="linkTreeContainer">
-                <a href="https://autonomaindustriale.bandcamp.com/" target="_blank" className="link"><p className="pLink">_Store</p></a>
-                <a href="https://soundcloud.com/autonoma-industriale" target="_blank" className="link"><p className="pLink">_Soundcloud</p></a>
-                <a href="https://soundcloud.com/autonoma-industriale/sets/mix-series" target="_blank" className="link"><p className="pLink">_Mix Series</p></a>
-                <a href="https://www.youtube.com/channel/UC_MYMo3Qpq7HZzDt0rz_d3Q" target="_blank" className="link"><p className="pLink">_Youtube</p></a>
-                <a href="https://www.facebook.com/AutonomaIndustriale" target="_blank" className="link"><p className="pLink">_Facebook</p></a>
-                <a href="https://www.instagram.com/autonoma.industriale/?hl=it" target="_blank" className="link"><p className="pLink">_Instagram</p></a>
+                <a href="https://autonomaindustriale.bandcamp.com/" target="_blank" className="link"><p className="pLink">Bandcamp</p></a>
+                <a href="https://soundcloud.com/autonoma-industriale" target="_blank" className="link"><p className="pLink">Soundcloud</p></a>
+                <a href="https://soundcloud.com/autonoma-industriale/sets/mix-series" target="_blank" className="link"><p className="pLink">Mix Series</p></a>
+                <a href="https://www.youtube.com/channel/UC_MYMo3Qpq7HZzDt0rz_d3Q" target="_blank" className="link"><p className="pLink">Youtube</p></a>
+                <a href="https://www.facebook.com/AutonomaIndustriale" target="_blank" className="link"><p className="pLink">Facebook</p></a>
+                <a href="https://www.instagram.com/autonoma.industriale/?hl=it" target="_blank" className="link"><p className="pLink">Instagram</p></a>
             </div>
         </div>
     )
